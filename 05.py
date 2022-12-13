2312f0c9d2c8dcbad3cd6150e903b900483c630d
Rareș Cosma <rares@getbetter.ro>
2022-12-13 12:37:55 +0200

    add days 2, 3, 4 and 5

---
+++ 05.py
@@ -0,0 +1,31 @@
+from collections import deque
+from pathlib import Path
+
+stack_lines = Path("inputs/05_s.txt").read_text().splitlines()
+
+stacks = []
+for line in stack_lines:
+    crates = line.split(" ")
+    crates.pop(0)
+    stacks.append(deque(crates))
+
+
+# NOTE: from_stack, to_stack are 1-indexed
+def move(how_many, from_stack, to_stack):
+    buf = deque([])
+    for _ in range(how_many):
+        buf.appendleft(stacks[from_stack - 1].pop())
+    stacks[to_stack - 1].extend(buf)
+
+
+move_lines = Path("inputs/05_i.txt").read_text().splitlines()
+
+moves = []
+for line in move_lines:
+    syms = line.split(" ")
+    move(int(syms[1]), int(syms[3]), int(syms[5]))
+
+print(stacks)
+answer = [(s[-1] if s else " ") for s in stacks]
+
+print("".join(answer))

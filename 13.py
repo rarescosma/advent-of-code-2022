1bbbff271fce701a32a9917b4838fabd219b861c
Rareș Cosma <rares@getbetter.ro>
2022-12-13 20:34:08 +0200

    day 13: simplify logic
    since we're implementing a comparator
    it suffices to return negative / 0 / positive values
    not necessarily -1/0/1
    
    also removed the type check indirection to please
    the almighty mypy

--- 13.py
+++ 13.py
@@ -2,7 +2,7 @@ from functools import cmp_to_key, reduce
 from operator import mul
 from pathlib import Path
 from textwrap import dedent
-from typing import Any
+from typing import Union
 
 real_data = Path("inputs/13.txt").read_text()
 
@@ -35,49 +35,36 @@ test_data = dedent(
 )
 
 
-def _is_int(thing: Any) -> bool:
-    return not isinstance(thing, list)
+def comp_terms(left_l: Union[int, list], right_l: Union[int, list]) -> int:
+    if isinstance(left_l, int):
+        if isinstance(right_l, list):
+            return comp_terms([left_l], right_l)
+        else:
+            return left_l - right_l
+    elif isinstance(right_l, int):
+        return comp_terms(left_l, [right_l])
 
-
-def comp_lists(left_l: list, right_l: list) -> int:
-    upper = min(len(left_l), len(right_l))
-    i = 0
-    while i < upper:
-        left_i, right_i = left_l[i], right_l[i]
-        left_i = [left_i] if _is_int(left_i) else left_i
-        right_i = [right_i] if _is_int(right_i) else right_i
-
-        if cmp := (
-            comp_ints(left_i[0], right_i[0])
-            if len(left_i) == len(right_i) == 1
-            and _is_int(left_i[0])
-            and _is_int(right_i[0])
-            else comp_lists(left_i, right_i)
-        ):
-            return cmp
-        i += 1
-    return comp_ints(len(left_l[i:]), len(right_l[i:]))
-
-
-def comp_ints(left: int, right: int) -> int:
-    return 1 if left < right else -1 if left > right else 0
+    for left_i, right_i in zip(left_l, right_l):
+        if _cmp := comp_terms(left_i, right_i):
+            return _cmp
+    return len(left_l) - len(right_l)
 
 
 # Part 1
 p1 = 0
+packets = []
 for _i, pairs in enumerate(real_data.split("\n\n")):
     parts = pairs.splitlines()
-    left_list = eval(parts[0])
-    right_list = eval(parts[1])
-    if res := comp_lists(left_list, right_list) == 1:
+    left_term, right_term = map(eval, parts)
+    packets.extend([left_term, right_term])
+    if comp_terms(left_term, right_term) < 0:
         p1 += _i + 1
 print(p1)
 
 # Part 2
-packets = [eval(p) for p in real_data.splitlines() if p]
 markers = [[[2]], [[6]]]
 packets.extend(markers)
 
-packets = sorted(packets, key=cmp_to_key(comp_lists), reverse=True)
+packets = sorted(packets, key=cmp_to_key(comp_terms))
 p2 = reduce(mul, [packets.index(_) + 1 for _ in markers])
 print(p2)

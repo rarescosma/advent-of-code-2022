85155dd5943ab798b2ced2a2a23dee20d46b86e9
Rareș Cosma <rares@getbetter.ro>
2022-12-15 16:57:32 +0200

    nits

--- 01.py
+++ 01.py
@@ -2,20 +2,18 @@ from pathlib import Path
 
 lines = Path("inputs/01.txt").read_text().splitlines()
 
-p1 = 0
 elves = []
 elf = 0
 for line in lines:
-    if not line:
+    if line:
+        elf += int(line)
+    else:
         elves.append(elf)
         elf = 0
-    else:
-        elf += int(line)
-    p1 = max(p1, elf)
 
 # Part 1
-print(p1)
+print(max(elves))
 
 # Part 2
-elves.sort(reverse=True)
-print(sum(elves[:3]))
+elves.sort()
+print(sum(elves[-3:]))

d899ca8f022d2c4dd1eb41d2455c4b5d7320b7b2
Rareș Cosma <rares@getbetter.ro>
2023-07-01 14:55:17 +0200

    fix: prefer rust solvers if built

--- par.sh
+++ par.sh
@@ -12,10 +12,10 @@ export -f locat
 for x in $(seq -w 1 $DAY); do
   ({
     echo ">>> Day $x <<<"
-    if test -f day$x.py; then
-      $INTERPRETER day$x.py
-    elif test -x target/release/day$x; then
+    if test -x target/release/day$x; then
       ./target/release/day$x 2>/dev/null
+    elif test -f day$x.py; then
+      $INTERPRETER day$x.py
     fi
     echo
   } | locat) &
